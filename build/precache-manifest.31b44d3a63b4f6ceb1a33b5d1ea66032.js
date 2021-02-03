869764b0fa36df492b04aa467ddf91cbba6f3521
Mikolaj Szymanski <szymikolaj@gmail.com>
2021-02-03 17:53:32 +0100

    readme

---
+++ build/precache-manifest.31b44d3a63b4f6ceb1a33b5d1ea66032.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd6768d956927ede8870f28289166bbb",
+    "url": "/filmder/index.html"
+  },
+  {
+    "revision": "34d535822f7b08158da7",
+    "url": "/filmder/static/css/2.a1074c3d.chunk.css"
+  },
+  {
+    "revision": "2055451d19079fde110f",
+    "url": "/filmder/static/css/main.e073ac00.chunk.css"
+  },
+  {
+    "revision": "34d535822f7b08158da7",
+    "url": "/filmder/static/js/2.e4fc5785.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/filmder/static/js/2.e4fc5785.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2055451d19079fde110f",
+    "url": "/filmder/static/js/main.79420a8b.chunk.js"
+  },
+  {
+    "revision": "e1798e524006ca1fd0c1",
+    "url": "/filmder/static/js/runtime-main.ef3db426.js"
+  }
+]);
